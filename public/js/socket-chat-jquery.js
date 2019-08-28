9ba1e997cc2eb84993d73db065f36827dd2a9602
LKingJ23 <lkingj23@gmail.com>
2019-08-28 17:06:35 +0200

    fixed error

--- public/js/socket-chat-jquery.js
+++ public/js/socket-chat-jquery.js
@@ -3,7 +3,8 @@ var params = new URLSearchParams(window.location.search);
 var nombre = params.get('nombre');
 var sala = params.get('sala');
 
-//Referencias jquery
+
+// referencias de jQuery
 var divUsuarios = $('#divUsuarios');
 var formEnviar = $('#formEnviar');
 var txtMensaje = $('#txtMensaje');
@@ -11,29 +12,35 @@ var divChatbox = $('#divChatbox');
 
 
 // Funciones para renderizar usuarios
-function renderizarUsuarios(personas) {
+function renderizarUsuarios(personas) { // [{},{},{}]
+
     console.log(personas);
+
     var html = '';
+
     html += '<li>';
     html += '    <a href="javascript:void(0)" class="active"> Chat de <span> ' + params.get('sala') + '</span></a>';
     html += '</li>';
 
     for (var i = 0; i < personas.length; i++) {
+
         html += '<li>';
-        html += '    <a data-id="' + personas[i].id + '" href="javascript:void(0)"><img src="assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>' + personas[i].nombre + '<small class="text-success">online</small></span></a>';
+        html += '    <a data-id="' + personas[i].id + '"  href="javascript:void(0)"><img src="assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>' + personas[i].nombre + ' <small class="text-success">online</small></span></a>';
         html += '</li>';
     }
 
     divUsuarios.html(html);
 
-};
+}
+
 
 function renderizarMensajes(mensaje, yo) {
-    var html = '';
-    var fecha = new Date();
-    var hora = fecha.getHours() + ':' + fecha.getMinutes();
-    var adminInfo = 'info';
 
+    var html = '';
+    var fecha = new Date(mensaje.fecha);
+    var hora = fecha.getHours() + ':' + fecha.getMinutes();
+
+    var adminClass = 'info';
     if (mensaje.nombre === 'Administrador') {
         adminClass = 'danger';
     }
@@ -47,20 +54,27 @@ function renderizarMensajes(mensaje, yo) {
         html += '    <div class="chat-img"><img src="assets/images/users/5.jpg" alt="user" /></div>';
         html += '    <div class="chat-time">' + hora + '</div>';
         html += '</li>';
+
     } else {
+
         html += '<li class="animated fadeIn">';
+
         if (mensaje.nombre !== 'Administrador') {
             html += '    <div class="chat-img"><img src="assets/images/users/1.jpg" alt="user" /></div>';
         }
+
         html += '    <div class="chat-content">';
         html += '        <h5>' + mensaje.nombre + '</h5>';
         html += '        <div class="box bg-light-' + adminClass + '">' + mensaje.mensaje + '</div>';
         html += '    </div>';
         html += '    <div class="chat-time">' + hora + '</div>';
         html += '</li>';
+
     }
 
+
     divChatbox.append(html);
+
 }
 
 function scrollBottom() {
@@ -81,27 +95,35 @@ function scrollBottom() {
 }
 
 
-//Listener
+
+
+// Listeners
 divUsuarios.on('click', 'a', function() {
+
     var id = $(this).data('id');
+
     if (id) {
         console.log(id);
     }
+
 });
 
 formEnviar.on('submit', function(e) {
+
     e.preventDefault();
 
     if (txtMensaje.val().trim().length === 0) {
         return;
-    } else {
-        socket.emit('crearMensaje', {
-            nombre: nombre,
-            mensaje: txtMensaje.val()
-        }, function(mensaje) {
-            txtMensaje.val('').focus();
-            renderizarMensajes(mensaje, true);
-            scrollBottom();
-        });
     }
+
+    socket.emit('crearMensaje', {
+        nombre: nombre,
+        mensaje: txtMensaje.val()
+    }, function(mensaje) {
+        txtMensaje.val('').focus();
+        renderizarMensajes(mensaje, true);
+        scrollBottom();
+    });
+
+
 });
